7cd69128de62d1b9ff50d721719a14206bb77aa9
Larry McNutt <larry.mcnutt@integritymarketing.com>
2022-10-30 13:25:53 -0500

    remove directory added to gitignore

--- build/src/module.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Set = exports.Client = void 0;
-const connection_1 = require("./connection");
-Object.defineProperty(exports, "Client", { enumerable: true, get: function () { return connection_1.RedisClient; } });
-const set_1 = require("./set");
-Object.defineProperty(exports, "Set", { enumerable: true, get: function () { return set_1.RedisSet; } });
